c62d63703da726e882284bfa2d3286b3a3170a4b
Javaria Brascom <javariab17@gmail.com>
2020-10-09 02:31:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,5 +6,5 @@ def join_nested_strings(src)
   while row_index =0 < src.count do
   element_index=0
   while element_index <src[row_index].count do
-string_of_string= src[]
+string_of_string= src[row_index][element_index]
 end
